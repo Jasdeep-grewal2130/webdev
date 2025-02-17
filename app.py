a45658b3b2e51613a0e9837937c5872ed60e620f
harman742 <harmansaini1209@gmail.com>
2025-02-17 03:28:32 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 @app.route('/')
+def home():
+    return render_template('index.html')
 
-def hello_world():
-  return render_template('index.html')
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0',debug=True)
+@app.route('/login')
+def login():
+    return render_template('loginwow.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
